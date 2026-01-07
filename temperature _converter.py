b3c4b04d14a2518761dc4417cac0a17371239857
sabrinakristagordo-schleiden <gordo.sabrina.schleiden@gmail.com>
2026-01-07 20:45:46 +0800

    Add files via upload

---
+++ temperature _converter.py
@@ -0,0 +1,17 @@
+# Temperature Converter Program
+# This program converts temperature from Celsius to Fahrenheit
+
+# Display program title
+print("Celsius to Fahrenheit Converter")
+
+# Ask the user to enter the temperature in Celsius
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Formula to convert Celsius to Fahrenheit
+# Multiply by 9, divide by 5, then add 32
+fahrenheit = (celsius * 9 / 5) + 32
+
+# Display the converted temperature
+print("Temperature in Fahrenheit:", fahrenheit)
+
+# End of program
